6629cc96246036df76a3139e816ae8508bcc7ad6
trialuser02 <trialuser02@users.noreply.github.com>
2026-09-02 18:11:15 +0000

    added qmmp-fix-cache-patch.patch
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/@13281

--- extras/package/win32/qmmp-2-win32.sh
+++ extras/package/win32/qmmp-2-win32.sh
@@ -78,6 +78,7 @@ build ()
 {
   QMMP_INSTALL_PREFIX=`dirs`/qmmp-distr 
   cd qmmp-${QMMP_VERSION}
+  cat ../../qmmp-fix-cache-patch.patch | patch -p0
   cmake . -G "MSYS Makefiles" -GNinja \
   -DCMAKE_BUILD_TYPE=Release \
   -DUSE_LIBRCD=ON -DUSE_DIR_ASSOC=OFF -DUSE_CONVERTER=OFF \
