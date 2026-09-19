7691068785db7196bfbf3d33fd937531afe6328d
desertmilk <deserthideout@tutamail.com>
2026-09-19 16:38:35 +0000

    feat: add showSettings slot to LibraryWidget for enhanced settings management

--- qmmp/src/plugins/General/library/librarywidget.h
+++ qmmp/src/plugins/General/library/librarywidget.h
@@ -54,6 +54,7 @@ private slots:
     void replaceSelected();
     void showTrackInformation();
     void showLibraryInformation();
+    void showSettings();
     void setArtistView();
     void setAlbumView();
     void setMostPlayedView();
